8a1b8d854dd4d851786fd45fbd3e405b4a79a019
Dex Skibidi <anhthinhcz@gmail.com>
2026-03-12 23:34:15 +0700

    Update loader URL in BossHop.lua

--- BossHop.lua
+++ BossHop.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f5effdfb4149843157fb3b4cee632f4b.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/582564af0139a0459d4c3a8a11913643.lua"))()
