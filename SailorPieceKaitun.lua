4393615ee823089a0a54795459ee166b49121c61
Dex Skibidi <anhthinhcz@gmail.com>
2026-04-08 22:24:27 +0700

    Update SailorPieceKaitun.lua

--- SailorPieceKaitun.lua
+++ SailorPieceKaitun.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vxezehub.click/files/api/loaders/wswlmt/SailoriPiece.lua.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/e2e2ad164b40bc75bd8d92fc3e97b10c.lua"))()
