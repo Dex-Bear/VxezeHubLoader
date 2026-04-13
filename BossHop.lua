47156803f9ddc1b87a2551e814bde0a3d66fd1c9
Dex Skibidi <anhthinhcz@gmail.com>
2026-04-14 01:32:11 +0700

    Update BossHop.lua

--- BossHop.lua
+++ BossHop.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/582564af0139a0459d4c3a8a11913643.lua"))()
+loadstring(game:HttpGet("https://vxezehub.click/files/api/loaders/62c605dd/BossFreeHop.lua.lua"))()
